4735d034514ebeb477077c7f03233231f0b5f5c8
Júlia Hartmann <juliahartmann@outlook.com.br>
2026-03-12 10:53:55 -0300

    Ajuste AssemblyInfo

--- JogoAdivinhacao.ConsoleApp/obj/Debug/net10.0/JogoAdivinhacao.ConsoleApp.AssemblyInfo.cs
+++ JogoAdivinhacao.ConsoleApp/obj/Debug/net10.0/JogoAdivinhacao.ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JogoAdivinhacao.ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ff033d2c6902cc5251200e729e275b0650d5b90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae07d0adffdfb87ad96c3c035a9386263907bdf1")]
 [assembly: System.Reflection.AssemblyProductAttribute("JogoAdivinhacao.ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JogoAdivinhacao.ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
